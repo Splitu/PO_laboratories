f68184f8bc5b27fa05eeaba0f987a921a0caaf6c
Splitu <bartosz.pekala1106@gmail.com>
2023-04-18 22:27:09 +0200

    Added files

---
+++ Lab3/Zadanie3.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class Zadanie3 {
+    public static void main(String[] args) {
+        Scanner read = new Scanner(System.in);
+        System.out.print("Podaj n: ");
+        int n = read.nextInt();
+        n--;
+        int t[] = new int[n];
+        int out = 0;
+        for (int i = 0; i < n; i++) {
+            t[i] = i + 1;
+            if (t[i] % 2 == 0)
+                out = out + t[i];
+        }
+        System.out.println("Suma liczb parzystych wynosi: " + out);
+    }
+}
